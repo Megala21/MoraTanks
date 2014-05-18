8927bf1fd0c3376463736c7801521f65c273fc71
Pranavan Theivendiram <234rana@gmail.com>
2014-05-18 16:52:54 +0530

    Deleted Files

--- src/MessageInterpretations/ReceiveInterpretation.java
+++ src/MessageInterpretations/ReceiveInterpretation.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package MessageInterpretations;
 
 import MapDetails.Brick;
@@ -26,8 +25,8 @@ import org.newdawn.slick.state.StateBasedGame;
  *
  * @author DELL
  */
-public class ReceiveInterpretation implements Observer{
-    
+public class ReceiveInterpretation implements Observer {
+
     private final String players_full = "PLAYERS_FULL#";
     private final String already_added = "ALREADY_ADDED#";
     private final String game_already_started = "GAME_ALREADY_STARTED#";
@@ -35,98 +34,96 @@ public class ReceiveInterpretation implements Observer{
     private StateBasedGame game;
     private Map map;
     private GameOpening gameOpening;
-   
-    public ReceiveInterpretation(StateBasedGame game, Map map){
-       this.game = game;
-       this.map = map;
-      
-       gameOpening = (GameOpening) game.getState(0);
+
+    public ReceiveInterpretation(StateBasedGame game, Map map) {
+        this.game = game;
+        this.map = map;
+
+        gameOpening = (GameOpening) game.getState(0);
     }
-    
-    public void decode(String message){
-        String reply= message;
-        
-       System.out.println(message);
-        if(reply.equalsIgnoreCase(players_full)){
+
+    public void decode(String message) {
+        String reply = message;
+
+        System.out.println(message);
+        if (reply.equalsIgnoreCase(players_full)) {
             game.enterState(3);
-        }
-        else if(reply.equalsIgnoreCase(already_added)){
+        } else if (reply.equalsIgnoreCase(already_added)) {
             gameOpening.indicateSituation("Player already added");
-        }
-        else if(reply.equalsIgnoreCase(game_already_started)){
+        } else if (reply.equalsIgnoreCase(game_already_started)) {
             game.enterState(0);
             gameOpening.indicateSituation("Player Full, try again later");
-        }
-        else if(reply.equalsIgnoreCase(game_finished)){
+        } else if (reply.equalsIgnoreCase(game_finished)) {
             game.enterState(2);
-        }
-        else if(reply.startsWith("S"))
+        } else if (reply.startsWith("S")) {
             createPlayer(reply);
-        else if(reply.startsWith("I"))
+        } else if (reply.startsWith("I")) {
             createMap(reply);
-        else if(reply.startsWith("G:"))
+        } else if (reply.startsWith("G:")) {
             updateMap(reply);
-        else if(reply.startsWith("C"))
+        } else if (reply.startsWith("C")) {
             updateCoin(reply);
-        else if(reply.startsWith("L"))
+        } else if (reply.startsWith("L")) {
             updateLife(reply);
+        }
     }
-    
-    public void createMap(String details){
-       String temp[] = details.split(":");
-       
-       System.out.println(details);
-      
-       
-       
-       if(Integer.parseInt(temp[1].substring(1)) == map.getIndex()) {
-       
+
+    public void createMap(String details) {
+        String temp[] = details.split(":");
+
+        System.out.println(details);
+
+        if (Integer.parseInt(temp[1].substring(1)) == map.getIndex()) {
+
             String brick[] = temp[2].split("[,]");
-             for(int i = 0; i < brick.length; i++)
+            for (int i = 0; i < brick.length; i++) {
                 System.out.println("brick " + brick[i]);
+            }
             String stone[] = temp[3].split("[;\\,\\]");
             String water[] = temp[4].split("[;\\,\\#\\]");
 
-            for(int i = 0; i < brick.length; i = i+2){
-                map.addBricks(new Brick(Integer.parseInt(brick[i]),Integer.parseInt(brick[i+1])));
+            for (int i = 0; i < brick.length; i = i + 2) {
+                map.addBricks(new Brick(Integer.parseInt(brick[i]), Integer.parseInt(brick[i + 1])));
             }
-            for(int i = 0; i < stone.length; i = i+2){
-                map.addStone(new Stone(Integer.parseInt(stone[i]),Integer.parseInt(stone[i+1])));
+            for (int i = 0; i < stone.length; i = i + 2) {
+                map.addStone(new Stone(Integer.parseInt(stone[i]), Integer.parseInt(stone[i + 1])));
             }
-            for(int i = 0; i < water.length; i = i+2){
-                map.addWater(new Water(Integer.parseInt(water[i]),Integer.parseInt(water[i+1])));
+            for (int i = 0; i < water.length; i = i + 2) {
+                map.addWater(new Water(Integer.parseInt(water[i]), Integer.parseInt(water[i + 1])));
             }
-       }
-       
-       if(game.getCurrentState().getID() != 1)
-           game.enterState(1);
+        }
+
+        if (game.getCurrentState().getID() != 1) {
+            game.enterState(1);
+        }
     }
 
     private void createPlayer(String reply) {
+
         String temp[] = reply.split("[:\\#]");
         int ind = 0;
-        
-       for( int i = 1 ; i < temp.length ; i++) {
-        String[] tmp = temp[i].split("[,\\;]");
-        int x = Integer.parseInt(tmp[1]);
-        System.out.println(x);
-        int y = Integer.parseInt(tmp[2]);
-        int direction = Integer.parseInt(tmp[3]);
-        int index = Integer.parseInt(tmp[0].substring(1));
-        
-        map.addPlayer(new Player(x, y, direction, index));
-        ind = index;
-       }
-       
-       map.setIndex(ind);
+
+        for (int i = 1; i < temp.length; i++) {
+            String[] tmp = temp[i].split("[,\\;]");
+            int x = Integer.parseInt(tmp[1]);
+            System.out.println(x);
+            int y = Integer.parseInt(tmp[2]);
+            int direction = Integer.parseInt(tmp[3]);
+            int index = Integer.parseInt(tmp[0].substring(1));
+
+            map.addPlayer(new Player(x, y, direction, index));
+            ind = index;
+        }
+
+        map.setIndex(ind);
         game.enterState(1);
-        
+
     }
 
     private void updateMap(String reply) {
         String temp[] = reply.split("[:\\#]");
-        
-        for(int i = 1; i < temp.length-1; i++){
+
+        for (int i = 1; i < temp.length - 1; i++) {
             String[] t = temp[i].split("[;\\,]");
             int x = Integer.parseInt(t[1]);
             int y = Integer.parseInt(t[2]);
@@ -136,13 +133,13 @@ public class ReceiveInterpretation implements Observer{
             int coins = Integer.parseInt(t[6]);
             int points = Integer.parseInt(t[7]);
             int index = Integer.parseInt(t[0].substring(1));
-            map.updatePlayer(x, y, direction, shot, health,coins, points, index);
+            map.updatePlayer(x, y, direction, shot, health, coins, points, index);
         }
-        String brick[] = temp[temp.length-1].split("[;\\,]");
-        
-        for(int i = 0;i< brick.length - 2;i = i+3){
-             map.updateBrick(i, i+1, i+2);
-                    
+        String brick[] = temp[temp.length - 1].split("[;\\,]");
+
+        for (int i = 0; i < brick.length - 2; i = i + 3) {
+            map.updateBrick(i, i + 1, i + 2);
+
         }
     }
 
@@ -160,7 +157,7 @@ public class ReceiveInterpretation implements Observer{
         int x = Integer.parseInt(life[1]);
         int y = Integer.parseInt(life[2]);
         long lifeTime = Long.parseLong(life[3]);
-        
+
         map.addLifePack(new LifePack(x, y, lifeTime));
     }
 
@@ -169,5 +166,4 @@ public class ReceiveInterpretation implements Observer{
         decode(String.valueOf(o1));
     }
 
- 
 }
