83417f6fc48be4df50c92015cec763bf1b81d9d7
Pranavan Theivendiram <234rana@gmail.com>
2014-05-20 15:06:12 +0530

    AI

--- src/MapDetails/BonusElement.java
+++ src/MapDetails/BonusElement.java
@@ -14,11 +14,6 @@ package MapDetails;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
-/**
- *
- * @author ishan
- */
 public class BonusElement {
 
     //time that the element should disaapear at
--- src/Network/SendMessage.java
+++ src/Network/SendMessage.java
@@ -6,12 +6,9 @@
 
 package Network;
 
-import MessageInterpretations.ReceiveInterpretation;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
